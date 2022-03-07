4d0d6833a20d169956fddf7150d3d196b26e444f
sgm28 <sakar.michel@gmail.com>
2022-03-07 10:45:18 -0500

    fix: Trying to locate bug
    Still searching for bug.

--- phpFunctions/register.php
+++ phpFunctions/register.php
@@ -3,7 +3,26 @@
 
 require "../DatabaseConnection/connectionForDB.php";
 
+try {
+ 
 
+
+ // prepare sql and bind parameters
+ $stmt = $conn->prepare("INSERT INTO users (firstname, lastname, email, usersPassword) VALUES (:firstname, :lastname, :email, :password)";
+ $stmt->bindParam(':firstname', $firstname);
+ $stmt->bindParam(':lastname', $lastname);
+ $stmt->bindParam(':email', $email);
+ $stmt->bindParam(':email', $password);
+
+
+  $stmt->execute();
+  echo "New record created successfully";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+
+$conn = null;
 
 ?>
 
