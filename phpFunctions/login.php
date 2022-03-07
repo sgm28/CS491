b33fa1d4183b1714e20541da145b05399420beb9
sgm28 <sgm28@njit.edu>
2022-03-07 17:47:12 -0500

    feat: Add password verify
    Connected to the database, retrieve the user information, checking to see if the user password match the hashed password.

--- phpFunctions/login.php
+++ phpFunctions/login.php
@@ -6,6 +6,40 @@
 	if(isset($_POST['submit']))
 	{
 		echo "Good";
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+
+
+
+		try {
+
+		$stmt = $conn->prepare("SELECT * FROM users WHERE email = :email");
+		$stmt->bindParam(':email', $email);
+		$stmt->execute();
+
+		 // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  $hashPassword = $result["usersPassword"]
+
+  if (password_verify($password, $hashPassword))
+  {
+  	echo "password match";
+  }
+  else {
+
+  	echo "password do not match";
+  }
+
+
+
+	}
+	catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+
+
 
 
 	}
@@ -13,5 +47,7 @@
 	{
 		echo "Not Good";
 	}
+
+
 
 ?>
