af7c325aaab612371789f940ca471a828936aa15
sgm28 <sakar.michel@gmail.com>
2022-03-07 10:35:58 -0500

    feat: Add userInput to database
    Gathering the userInput from the form that was submitted and adding the userInput to the database.

---
+++ phpFunctions/register.php
@@ -0,0 +1,38 @@
+<?php
+
+
+require "../DatabaseConnection/connectionForDB.php"
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+
+
+
+
+ // prepare sql and bind parameters
+ $stmt = $conn->prepare("INSERT INTO users (firstname, lastname, email, usersPassword) VALUES (:firstname, :lastname, :email, :password)";
+ $stmt->bindParam(':firstname', $firstname);
+ $stmt->bindParam(':lastname', $lastname);
+ $stmt->bindParam(':email', $email);
+ $stmt->bindParam(':email', $password);
+
+
+  $stmt->execute();
+  echo "New record created successfully";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+
+$conn = null;
+
+?>
+
+
+
+  
