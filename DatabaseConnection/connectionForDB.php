477263ccf1201e41a1f81ef9c8a17ee5181980d5
sgm28 <sakar.michel@gmail.com>
2022-03-07 09:47:09 -0500

    fix: Change DB name
    Forgot to change DB name to the name of the heroku database

--- DatabaseConnection/connectionForDB.php
+++ DatabaseConnection/connectionForDB.php
@@ -2,9 +2,10 @@
 $servername = "us-cdbr-east-05.cleardb.net";
 $username = "b241659a762e01";
 $password = "8ff51cab";
+$db = "heroku_d060d81cfdb8333"
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
