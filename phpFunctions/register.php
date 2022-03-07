c078361c7f8bb864e9c21ac38baa9d14cdb13fc5
sgm28 <sakar.michel@gmail.com>
2022-03-07 11:01:15 -0500

    fix: Added catch statement
    Add catch statement. Hopefully, I will not get the HTTP500 error

--- phpFunctions/register.php
+++ phpFunctions/register.php
@@ -10,7 +10,12 @@ try {
  // prepare sql and bind parameters
  $stmt = $conn->prepare("INSERT INTO users (firstname, lastname, email, usersPassword) VALUES (:firstname, :lastname, :email, :password)");
 
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
 
+
+$conn = null;
 
 ?>
 
