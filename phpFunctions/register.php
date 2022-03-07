cd9b1763c31aecc7c5419f10d9a4c9a9115d574b
sgm28 <sgm28@njit.edu>
2022-03-07 12:12:29 -0500

    feat: Add hash password
    Hash the user password and added salt

--- phpFunctions/register.php
+++ phpFunctions/register.php
@@ -10,10 +10,18 @@ try {
  // prepare sql and bind parameters
  $stmt = $conn->prepare("INSERT INTO users (firstname, lastname, email, usersPassword) VALUES (:firstname, :lastname, :email, :password)");
  $stmt->bindParam(':firstname', $_SESSION["firstName"]);
- $stmt->bindParam(':lastname', $lastname);
- $stmt->bindParam(':email', $email);
- $stmt->bindParam(':password', $password);
+ $stmt->bindParam(':lastname', $_SESSION["lastName"]);
+ $stmt->bindParam(':email', $_SESSION["email"]);
 
+
+$hashPassword = password_hash($_SESSION["password"], PASSWORD_DEFAULT);
+
+
+$stmt->bindParam(':password', $hashPassword);
+
+
+
+        
 
   $stmt->execute();
   echo "New record created successfully";
