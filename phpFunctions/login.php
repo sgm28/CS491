f96a7cecd7fc1b797e0da060c555c8c77612be16
sgm28 <sgm28@njit.edu>
2022-03-07 19:37:30 -0500

    fix: trying to print result from DB

--- phpFunctions/login.php
+++ phpFunctions/login.php
@@ -9,51 +9,25 @@
 		$email = $_POST['email'];
 		$password = $_POST['password'];
 
-		var_dump($email);
-		var_dump($password);
-
 
 
 
 		try {
 
 		$stmt = $conn->prepare("SELECT * FROM users WHERE email = :email");
-		$stmt->bindParam(':email', "a@gmail.com");
+		$stmt->bindParam(':email', $email);
 		$stmt->execute();
 
 
-		 // set the resulting array to associative
-  $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-//print_r($result);
-//print("\n");
-//print_r($stmt);
-
-  if(empty($result)){
-
-  	echo " result are empty";
-  }
-  $var_dump($result['email']);
-  print_r($result);
-  $hashPassword = $result['usersPassword'];
-  var_dump($hashPassword);
-  var_dump($password);
-
-  if (password_verify($password, $hashPassword))
-  {
-  	echo "password match";
-  }
-  else {
-
-  	echo "password do not match";
-  }
+  		$result = $stmt->fetch(PDO::FETCH_ASSOC);
 
 
-
+		print_r($result);
+		print("\n");
 	}
 	catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
-}
+  			echo $sql . "<br>" . $e->getMessage();
+	}
 
 
 
