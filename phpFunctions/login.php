c3edab3dbc15bf232937f435499cf1d4a4b13c0c
sgm28 <sgm28@njit.edu>
2022-03-10 22:12:37 -0500

    feat: Add Logic to redirect user
    Add logic to redirect user based on user_type

--- phpFunctions/login.php
+++ phpFunctions/login.php
@@ -45,11 +45,11 @@ $_SESSION["firstName"] = $result['firstname'];
 
 	if ($result['user_type'] == "admin")
 	{
-		  //header("location ./adminHome.php");
+		  header("location: ./adminHome.php");
 	}
 	else {
 
-			//header("location: ../home.php");
+			header("location: ../home.php");
 	}
 
   	
